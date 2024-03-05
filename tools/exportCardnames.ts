78af083aa0795980f237d12c7899c9c0a227d1b1
takanari-saiki <stakanari987@gmail.com>
2024-03-05 14:57:33 +0900

    カード名一覧jsonの形式を変更

--- tools/exportCardnames.ts
+++ tools/exportCardnames.ts
@@ -8,14 +8,17 @@ main()
 
 async function main() {
   const cardRecords = await getAll(
-    'SELECT max(id) as id, name from cards'
+    'SELECT id, name from cards'
     + ' WHERE name IS NOT NULL'
-    + ' GROUP BY name'
-    + " HAVING name != ''"
+    + ' ORDER BY name'
   )
   const cardNameIdMap = {}
   for (const record of cardRecords) {
-    cardNameIdMap[record.name] = record.id
+    if (record.name in cardNameIdMap) {
+      cardNameIdMap[record.name].push(record.id)
+    } else {
+      cardNameIdMap[record.name] =[ record.id ]
+    }
   }
   writeFileSync('cardnames.json', JSON.stringify(cardNameIdMap, null, 2));
 }
